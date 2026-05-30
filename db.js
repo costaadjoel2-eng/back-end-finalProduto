d1b0c007ced80c2b24644478e2081e13eae39ced
costaadjoel2-eng <costaadjoel2@gmail.com>
2026-05-30 14:41:29 +0000

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'stp_produtos'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("❌ Erro ao conectar ao MySQL:", err.message);
+    } else {
+        console.log("✅ MySQL conectado com sucesso!");
+    }
 });
 
 module.exports = db;
